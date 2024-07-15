737693e28e47fb1240da775a6ad3c4c0e09d8379
Tyler Natsuhara <ty@ty.pizza>
2024-07-14 21:04:43 -0700

    render multiple frames

--- src/components/FrameRenderer.tsx
+++ src/components/FrameRenderer.tsx
@@ -1,20 +1,48 @@
 import { Component, Show, createEffect, createSignal } from 'solid-js'
-import { getFrame } from '../utils/video'
+import { getDurationSeconds, getFrame } from '../utils/video'
+
+const FRAMES = 10
+const TRIM_START_SECONDS = 5
+const TRIM_END_SECONDS = 5
 
 export const FrameRenderer: Component<{ file: File | undefined }> = (props) => {
-  const [buf, setBuf] = createSignal<ArrayBuffer>()
+  const [frames, setFrames] = createSignal<string[]>([]) // base64 image strings
+  const [duration, setDuration] = createSignal<number>()
 
   createEffect(() => {
     if (props.file) {
-      getFrame(props.file!, 300)
-        .then((blob) => blob.arrayBuffer())
-        .then((buffer) => setBuf(buffer))
+      const startTime = new Date().getTime()
+      setFrames([])
+
+      getDurationSeconds(props.file).then((seconds) => {
+        setDuration(seconds)
+
+        const segmentLength = (seconds - TRIM_START_SECONDS - TRIM_END_SECONDS) / (FRAMES - 1)
+
+        const framePromises = Array.from(
+          { length: FRAMES },
+          (v, k) => TRIM_START_SECONDS + segmentLength * k
+        ).map(async (timestamp) => {
+          const blob = await getFrame(props.file!, timestamp)
+          const buffer = await blob.arrayBuffer()
+          return bufferToBase64(buffer)
+        })
+
+        Promise.all(framePromises).then((frames) => {
+          setFrames(frames)
+          const timeTaken = new Date().getTime() - startTime
+          console.log(`rendered frames after ${timeTaken} milliseconds`)
+        })
+      })
     }
   })
 
   return (
-    <Show when={buf()}>
-      <img id="ItemPreview" src={`data:image/png;base64,${bufferToBase64(buf()!)}`} />
+    <Show when={frames().length > 0}>
+      <div>duration: {duration()}</div>
+      {frames().map((base64) => (
+        <img id="ItemPreview" style={{ width: '300px' }} src={`data:image/png;base64,${base64}`} />
+      ))}
     </Show>
   )
 }
--- src/utils/video.ts
+++ src/utils/video.ts
@@ -1,12 +1,25 @@
-export const getDuration = (file: File) => {}
+export const getDurationSeconds = (file: File): Promise<number> => {
+  return new Promise((resolve, reject) => {
+    const player = document.createElement('video')
+    player.setAttribute('src', URL.createObjectURL(file))
+    player.load()
+    player.onerror = () => {
+      reject('error loading video file')
+    }
+
+    player.onloadedmetadata = () => {
+      resolve(player.duration)
+    }
+  })
+}
 
 export const getFrame = (file: File, timestamp = 0): Promise<Blob> => {
   return new Promise((resolve, reject) => {
     const player = document.createElement('video')
     player.setAttribute('src', URL.createObjectURL(file))
     player.load()
-    player.onerror = (e) => {
-      reject('error when loading video file')
+    player.onerror = () => {
+      reject('error loading video file')
     }
 
     player.onloadedmetadata = () => {
@@ -32,7 +45,7 @@ export const getFrame = (file: File, timestamp = 0): Promise<Blob> => {
             resolve(blob!)
           },
           'image/jpeg',
-          0.75 /* quality */
+          1 /* quality */
         )
       }
 
@@ -41,48 +54,5 @@ export const getFrame = (file: File, timestamp = 0): Promise<Blob> => {
         player.currentTime = timestamp
       }, 200)
     }
-  })
-}
-
-export const getVideoCover = (file: File, seekTo = 0.0) => {
-  return new Promise((resolve, reject) => {
-    // load the file to a video player
-    const videoPlayer = document.createElement('video')
-    videoPlayer.setAttribute('src', URL.createObjectURL(file))
-    videoPlayer.load()
-    videoPlayer.addEventListener('error', (ex) => {
-      reject('error when loading video file', ex)
-    })
-    // load metadata of the video to get video duration and dimensions
-    videoPlayer.addEventListener('loadedmetadata', () => {
-      // seek to user defined timestamp (in seconds) if possible
-      if (videoPlayer.duration < seekTo) {
-        reject('video is too short.')
-        return
-      }
-      // delay seeking or else 'seeked' event won't fire on Safari
-      setTimeout(() => {
-        videoPlayer.currentTime = seekTo
-      }, 200)
-      // extract video thumbnail once seeking is complete
-      videoPlayer.addEventListener('seeked', () => {
-        console.log('video is now paused at %ss.', seekTo)
-        // define a canvas to have the same dimension as the video
-        const canvas = document.createElement('canvas')
-        canvas.width = videoPlayer.videoWidth
-        canvas.height = videoPlayer.videoHeight
-        // draw the video frame to canvas
-        const ctx = canvas.getContext('2d')
-        ctx.drawImage(videoPlayer, 0, 0, canvas.width, canvas.height)
-        // return the canvas image as a blob
-        ctx.canvas.toBlob(
-          (blob) => {
-            resolve(blob)
-          },
-          'image/jpeg',
-          0.75 /* quality */
-        )
-      })
-    })
   })
 }
