31dd2016ee951d69d565e19c6a18b9eed4dbea6e
Tyler Natsuhara <ty@ty.pizza>
2024-07-28 18:36:32 -0700

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
-export default () => {
-  return defineConfig({
-    plugins: [
-      /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-      // devtools(),
-      solidPlugin(),
-    ],
-    server: {
-      port: 3000,
-    },
-    build: {
-      target: 'esnext',
-    },
-  })
-}
+export default defineConfig({
+  plugins: [solidPlugin()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+  // the project is deployed at ty.pizza/frames, so we need to configure the base path
+  base: process.env.NODE_ENV === 'production' ? 'frames/' : '/',
+})
